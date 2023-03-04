a7e5f5cf909f0a9a6e38b811542a45b7b14d5c59
vriti2k <vriti.kumar@outlook.com>
2023-03-04 23:04:18 +0000

    add ability to print blue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("I'm Printing Green!");
 Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I'm Printing Blue!");
+Console.ResetColor();
